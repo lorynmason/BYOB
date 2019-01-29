43400d983a751860d565f0180b8dcae14e1bbf63
Ashley Levi <Ashplevi@gmail.com>
2019-01-29 13:32:37 -0700

    Fixes issue #7, add post endpoint to post a new brewery

--- server.js
+++ server.js
@@ -60,6 +60,28 @@ app.get('/api/beers/:beer_id', (request, response) => {
   })
 });
 
+app.post('/api/breweries', (request, response) => {
+  const { brewery } = request.body;
+
+  for (let requiredParameter of ['name', 'city', 'food', 'dog_friendly', 'outdoor_seating', 'website']){
+    if (!brewery[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: {name: <string>, city: <string>, food: <string>, dog_friendly: <string>, outdoor_seating: <string>, website: <string>}. You're missing a "${requiredParameter}" property`})
+    }
+  }
+
+  database('breweries').insert(brewery, 'id')
+    .then(breweryId => {
+      response.status(201).json({...brewery, id: breweryId[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error})
+    });
+})
+
+
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
