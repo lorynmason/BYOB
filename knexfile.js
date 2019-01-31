6cf60f3936f116e99d6a8c3032659794976e0367
Ashley Levi <Ashplevi@gmail.com>
2019-01-31 11:02:27 -0700

    Update knex file to add production

--- knexfile.js
+++ knexfile.js
@@ -20,5 +20,13 @@ module.exports = {
       directory: './db/seeds/test'
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 };
